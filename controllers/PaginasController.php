edf4edc65d77abf13830df9a59a84b6d0374652c
gilmarinoj <gilmarinoj@gmail.com>
2025-02-06 15:12:13 -0400

    first commit

---
+++ controllers/PaginasController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Controllers;
+
+use Model\Categoria;
+use Model\Dia;
+use Model\Evento;
+use Model\Hora;
+use Model\Ponente;
+use Model\Usuario;
+use MVC\Router;
+
+class PaginasController {
+
+    public static function index(Router $router) {
+        $router->render('paginas/index', [
+            'titulo' => 'Inicio'
+        ]);
+    }
+
+    public static function sobreNosotros(Router $router) {
+        $router->render('paginas/sobre-nosotros', [
+            'titulo' => 'Acerca de EventConnect'
+        ]);
+    }
+
+    public static function pases(Router $router) {
+        $router->render('paginas/pases', [
+            'titulo' => 'Pases EventConnect'
+        ]);
+    }
+
+    public static function conferencias(Router $router) {
+
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+        $eventos_formateados = [];
+
+        foreach($eventos as $evento) {
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+
+            if($evento->dia_id === '1' && $evento->categoria_id === '1') {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === '2' && $evento->categoria_id === '1') {
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            if($evento->dia_id === '1' && $evento->categoria_id === '2') {
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+
+            if($evento->dia_id === '2' && $evento->categoria_id === '2') {
+                $eventos_formateados['workshops_s'][] = $evento;
+            }
+        }
+
+
+        $router->render('paginas/conferencias', [
+            'titulo' => 'Workshops & Conferencias',
+            'eventos' => $eventos_formateados,
+        ]);
+    }
+}
---
+++ views/paginas/conferencias.php
@@ -0,0 +1,61 @@
+<main class="agenda">
+    <h2 class="agenda__heading"><?php echo $titulo; ?></h2>
+    <p class="agenda__descripcion">Workshops y Conferencias dictados por los expertos de EventConnect</p>
+
+    <div class="eventos">
+        <h3 class="eventos__heading">&lt;Conferencias /></h3>
+        <p class="eventos__fecha">Viernes 7 de Febrero</p>
+
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['conferencias_v'] as $evento) { ?>
+                    <div class="evento swiper-slide">
+                        <p class="evento__hora"><?php echo $evento->hora->hora; ?></p>
+
+                        <div class="evento__informacion">
+                            <h4 class="evento__nombre"><?php echo $evento->nombre; ?></h4>
+
+                            <p class="evento__introduccion"><?php echo $evento->descripcion; ?></p>
+
+                            <div class="evento__autor-info">
+                                <picture>
+                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.webp" type="image/webp">
+                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" type="image/png">
+                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
+                                </picture>
+
+                                <p class="evento__autor-nombre">
+                                    <?php echo $evento->ponente->nombre . ' ' . $evento->ponente->apellido; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+
+        <p class="eventos__fecha">Sabado 8 de Febrero</p>
+
+        <div class="eventos__listado">
+
+        </div>
+    </div>
+
+    <div class="eventos eventos--workshops">
+        <h3 class="eventos__heading">&lt;Workshops /></h3>
+        <p class="eventos__fecha">Viernes 7 de Febrero</p>
+
+        <div class="eventos__listado">
+
+        </div>
+
+        <p class="eventos__fecha">Sabado 8 de Febrero</p>
+
+        <div class="eventos__listado">
+
+        </div>
+    </div>
+</main>
